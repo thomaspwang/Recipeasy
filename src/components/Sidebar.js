87e854dbb8b546e63f56a6132e23f6e9216eed31
Jenna Bustami <jennabustami@berkeley.edu>
2021-12-06 20:43:48 -0800

    better side bar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,29 +1,68 @@
-import React from 'react';
-import Search from "./Search.js"
+import React, { useState } from "react";
+import Search from "./Search.js";
+//import react pro sidebar components
 import {
-  CDBSidebar,
-  CDBSidebarContent,
-  CDBSidebarHeader,
-} from 'cdbreact';
+  ProSidebar,
+  Menu,
+  MenuItem,
+  SidebarHeader,
+  SidebarFooter,
+  SidebarContent,
+} from "react-pro-sidebar";
+
+//import icons from react icons
+import { FaList, FaRegHeart } from "react-icons/fa";
+import { FiHome, FiLogOut, FiArrowLeftCircle, FiArrowRightCircle } from "react-icons/fi";
+import { RiPencilLine } from "react-icons/ri";
+import { BiCog } from "react-icons/bi";
+
+
+//import sidebar css from react-pro-sidebar module and our custom css 
+import "react-pro-sidebar/dist/css/styles.css";
+import "./Sidebar.css";
+
 
 const Sidebar = () => {
+  
+    //create initial menuCollapse state using useState hook
+    const [menuCollapse, setMenuCollapse] = useState(false)
+
+    //create a custom function that will change menucollapse state from false to true and true to false
+  const menuIconClick = () => {
+    //condition checking to change state from true to false and vice versa
+    menuCollapse ? setMenuCollapse(false) : setMenuCollapse(true);
+  };
+
   return (
-    <div style={{ display: 'flex', height: '100vh', overflow: 'scroll initial' }}>
-      <CDBSidebar textColor="green" backgroundColor="rgb(45,178,70, 0.1)" >
-        <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
-          <a href="/" className="text-decoration-none" style={{ color: 'inherit' }}>
-            Ingredients
-          </a>
-        </CDBSidebarHeader>
-
-        <CDBSidebarContent className="sidebar-content">
-    
-          <Search />
-    
-        </CDBSidebarContent>
-
-      </CDBSidebar>
-    </div>
+    <>
+      <div id="header">
+          {/* collapsed props to change menu size using menucollapse state */}
+        <ProSidebar collapsed={menuCollapse}>
+          <SidebarHeader>
+          <div className="logotext">
+              {/* small and big change using menucollapse state */}
+              <p>{menuCollapse ? "Add" : "Add Ingredients"}</p>
+            </div>
+            <div className="closemenu" onClick={menuIconClick}>
+                {/* changing menu collapse icon on click */}
+              {menuCollapse ? (
+                <FiArrowRightCircle/>
+              ) : (
+                <FiArrowLeftCircle/>
+              )}
+            </div>
+          </SidebarHeader>
+          <SidebarContent>
+          <p>{menuCollapse ? "" : <Search /> }</p>
+          </SidebarContent>
+          <SidebarFooter>
+            <Menu iconShape="square">
+              <MenuItem icon={<FiLogOut />}>Logout</MenuItem>
+            </Menu>
+          </SidebarFooter>
+        </ProSidebar>
+      </div>
+    </>
   );
 };
 
