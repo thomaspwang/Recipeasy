4f9cc181f15134eeeacd8497d7680b2c4ba92b2e
Amanda <amzhang@berkeley.edu>
2021-12-06 16:58:39 -0800

    login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,13 +1,52 @@
 import React from "react";
-  
-const Login = () => {
-  return (
-    <div>
-      <h1>
-        login/signup
-      </h1>
-    </div>
-  );
-};
-  
+import './login.css';
+
+// const Login = () => {
+//   return (
+//     <div>
+//       <h1>
+//         login/signup
+//       </h1>
+//     </div>
+//   );
+// };
+
+
+
+
+
+// import {ReactComponent as Logo} from '../../assets/instagram.svg'
+
+class Login extends React.Component{
+    state={
+        email:'',
+        pwd:''
+    }
+
+    handleChange = (e) =>{
+        const {name,value} = e.target
+        this.setState({[name]:value})
+    }
+
+    handleSubmit = (e) =>{
+        e.preventDefault()
+        this.props.isLogin(true)
+    }
+    render(){
+        return(
+            <div className='div-login'>
+                <div className='div-login-logo'>
+                </div>
+                <div>
+                    <form onSubmit = {this.handleSubmit}>
+                        <input className="inputLogin" type='email' name='email' placeholder='email...' required onChange={this.handleChange}/>
+                        <input className="inputLogin" type='password' name='pwd' placeholder='password...' required onChange={this.handleChange}/>
+                        <button className="buttonLogin" onSubmit={this.handleSubmit}>Log In</button>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
+
 export default Login;
